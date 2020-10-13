5b5aa2b26f8785ad0d2469e46afa8153cfd62031
Zixuan Wei <zixuan.wei@intel.com>
2020-10-13 18:44:41 +0800

    :sparkles: list node: problem 24

---
+++ problems/24-swap-nodes-in-pairs/answer.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include <ListNode.hpp>
+
+class Solution {
+public:
+  ListNode *swapPairs(ListNode *head) {
+    ListNode *new_head = head;
+
+    ListNode *first = head;
+    ListNode *end = nullptr;
+
+    while (first) {
+      ListNode *second = first->next;
+      if (end)
+        end->next = second;
+      if (!second) {
+        if (end) end->next = first;
+        break;
+      }
+      ListNode *third = second->next;
+
+      if (new_head == head) {
+        new_head = second;
+      }
+      second->next = first;
+      first->next = third;
+      end = first;
+
+      first = third;
+    }
+    return new_head;
+  }
+};
