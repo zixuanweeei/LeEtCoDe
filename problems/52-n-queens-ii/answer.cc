257294f1d01d08267d1c06a434a39c0a008a625c
Zixuan Wei <zixuan.wei@intel.com>
2020-10-17 11:38:45 +0800

    :sparkles: backtracing: problem 52 - n queens ii

---
+++ problems/52-n-queens-ii/answer.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+  void backtracing(const int y, const int n) {
+    if (y == n) {
+      count++;
+      return;
+    }
+    for (int x = 0; x < n; ++x) {
+      if (column[x] || diag1[x + y] || diag2[x - y + n - 1]) {
+        continue;
+      }
+      column[x] = diag1[x + y] = diag2[x - y + n - 1] = 1;
+      backtracing(y + 1, n);
+      column[x] = diag1[x + y] = diag2[x - y + n - 1] = 0;
+    }
+  }
+
+  int totalNQueens(int n) {
+    count = 0;
+    column.assign(n, 0);
+    diag1.assign(n + n, 0);
+    diag2.assign(n + n, 0);
+    backtracing(0, n);
+    return count;
+  }
+  int count;
+  std::vector<int> column;
+  std::vector<int> diag1;
+  std::vector<int> diag2;
+};
+
+int main(int argv, char **argc) {
+  int n = std::stoi(argc[1]);
+  Solution sol;
+  int ret = sol.totalNQueens(n);
+  std::cout << "Result is: " << ret << std::endl;
+
+  return 0;
+}
