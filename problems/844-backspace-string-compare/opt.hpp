3c1c580c9de604c46d7b4c4234236c4e5f3521d1
Zixuan Wei <zixuan.wei@intel.com>
2020-10-19 20:14:21 +0800

    :zap: [Easy] optimal solution: double pointers to P844

---
+++ problems/844-backspace-string-compare/opt.hpp
@@ -0,0 +1,41 @@
+#include <string>
+
+class OptSolution {
+public:
+  bool backspaceCompare(std::string S, std::string T) {
+    int i = S.length() - 1, j = T.length() - 1;
+    int skipS = 0, skipT = 0;
+
+    while (i >= 0 || j >= 0) {
+      while (i >= 0) {
+        if (S[i] == '#') {
+          skipS++, i--;
+        } else if (skipS > 0) {
+          skipS--, i--;
+        } else {
+          break;
+        }
+      }
+      while (j >= 0) {
+        if (T[j] == '#') {
+          skipT++, j--;
+        } else if (skipT > 0) {
+          skipT--, j--;
+        } else {
+          break;
+        }
+      }
+      if (i >= 0 && j >= 0) {
+        if (S[i] != T[j]) {
+          return false;
+        }
+      } else {
+        if (i >= 0 || j >= 0) {
+          return false;
+        }
+      }
+      i--, j--;
+    }
+    return true;
+  }
+};
