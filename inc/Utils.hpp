fba66249137e43e6aeae394dbb5f9fb6b565b603
Zixuan Wei <zixuan.wei@intel.com>
2020-10-26 20:43:35 +0800

    :bug: Uses const-reference

--- inc/Utils.hpp
+++ inc/Utils.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 template <typename T>
-void print_vector(std::vector<T> &vec) {
+void print_vector(const std::vector<T> &vec) {
   std::cout << "[";
   for (size_t i = 0; i < vec.size() - 1; ++i) std::cout << vec[i] << ", ";
   std::cout << vec.back() << "]\n";
